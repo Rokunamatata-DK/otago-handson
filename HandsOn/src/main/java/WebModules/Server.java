539bb9b2d49f6b1305f7879e30ee89836ae66530
findo360 <akatsuki1091@gmail.com>
2020-04-23 21:53:56 +1200

    Added dependency for h2 server

--- HandsOn/src/main/java/WebModules/Server.java
+++ HandsOn/src/main/java/WebModules/Server.java
@@ -9,6 +9,27 @@ package WebModules;
  *
  * @author Dominic
  */
+
+/**
 public class Server {
-    
+        public static void main(String[] args) throws Exception {
+        System.out.println("\nStarting Server.");
+
+        Server server = new Server();
+
+        CompletableFuture.runAsync(() -> {
+            server.start();
+        });
+
+        server.onStarted(() -> {
+            System.out.println("\nPress Enter to stop the server");
+        });
+
+        //wait for user to hit the enter key
+        System.in.read();
+        System.exit(0);
+    }
+
 }
+* 
+*/
